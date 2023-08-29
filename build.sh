c47be09b478351cd0af558ea1a8b487e8cc64a14
inovue <inouve3@gmail.com>
2023-08-29 10:37:57 +0900

    fix: build script

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 npx --yes playwright install --with-deps chromium
 
-rm -rf ~/public/shiki && mkdir -p ./public/shiki && cp -r ./node_modules/shiki/{dist,languages,samples,themes} ./public/shiki/
+rm -rf public/shiki && mkdir -p public/shiki && cp -r node_modules/shiki/{dist,languages,samples,themes} public/shiki/
