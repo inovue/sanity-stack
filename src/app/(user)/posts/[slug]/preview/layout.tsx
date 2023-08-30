e086ee3265056c2f134a9da009a4c5b9738c7df3
inovue <inouve3@gmail.com>
2023-08-30 18:38:15 +0900

    fix: preview render required token

--- src/app/(user)/posts/[slug]/preview/layout.tsx
+++ src/app/(user)/posts/[slug]/preview/layout.tsx
@@ -7,12 +7,16 @@ import { redirect } from 'next/navigation'
 export default async function PostPreviewLayout({children, params}: {children: React.ReactNode, params: {slug: string}}) {
   const preview = draftMode().isEnabled ? {token: readToken} : undefined
   //const preview = {token: readToken}
-
+  console.log('preview', preview)
   if (!preview) redirect(`/posts/${params.slug}`);
 
   return (
-    <PreviewProvider token={preview.token}>
-      {children}
-    </PreviewProvider>
+    <>
+      {preview && 
+        <PreviewProvider token={preview.token}>
+          {children}
+        </PreviewProvider>
+      }
+    </>
   )
 }
